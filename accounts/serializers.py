75db30828fe75c38df392d4ad2d2b6af9111b2c1
Esteban Carter <carterea1999@gmail.com>
2021-06-29 15:48:49 -0400

    added permissions

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
+    # has_owner_permissions = serializers.SerializerMethodField('get_owner_status')
+    # owner = serializers.ReadOnlyField(source='user.username')
+
+    # def get_owner_status(self, obj):
+    #     return obj.user == self.context['request'].user
     class Meta:
         model = Profile
         fields = '__all__'
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
+from .permissions import IsAuthOrReadOnly
 
 # Create your views here.
 class ProfileListAPIView(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ProfileListDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
